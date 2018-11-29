37ce937a0ebd8f360db9b71d7b57d53ceead0929
Gregor Bückendorf <gregor.bueckendorf@ias.rwth-aachen.de>
2018-11-29 14:07:37 +0100

    Added an error message and hopefully cut on unnecessary reloads

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var fadeTime = 300; // half of complete fade
-var refreshTime = 10000; // 5 minutes
+var refreshTime = 15000; // 15 seconds
 var csvUrl = './tapeout-progress.csv';
 
 function loadContent() {
@@ -8,16 +8,20 @@ function loadContent() {
     dataType: 'text',
     async: false,
     cache: false,
+    ifModified: true,
     success: function(data) {
       if (data) {
         $('.generated').fadeOut(fadeTime, function() { $(this).remove(); })
-        setTimeout( function(meters) {
+        setTimeout( function() {
           for ( let meter of processCSV(data) ) {
             createMeter(meter[0], meter[1]);
           }
-        }, fadeTime, data );
+        }, fadeTime );
+      } else {
+        createError();
       }
-    }
+    },
+    error: createError
   });
 }
 
@@ -31,22 +35,23 @@ function processCSV(text) {
 
 function createMeter(meterName, meterValue) {
   var meterPercentage = ( Number.parseFloat(meterValue) * 100 ).toFixed(0) + '%';
-  var progressLabel = $('<label/>', { class: 'generated', text: meterName } )
-  var progressBar = $('<div/>', { class: 'meterbar generated' } );
-  progressBar.append( $('<progress/>', { value: meterValue } ) );
+  var progressLabel = $('<label/>', { class: 'generated', text: meterName });
+  var progressBar = $('<div/>', { class: 'meterbar generated' });
+  progressBar.append( $('<progress/>', { value: meterValue }) );
   progressBar.append( $('<div/>', { class: 'percentage', text: meterPercentage }) );
   progressLabel.hide().appendTo('body').fadeIn(fadeTime);
   progressBar.hide().appendTo('body').fadeIn(fadeTime);
 }
 
-function populateGrid() {
-  $('.meter').fadeOut(fadeTime, function() { $(this).remove(); })
-  setTimeout('createMeters()', fadeTime);
+function createError() {
+  var errorMessage = 'Error: could not load tapeout progress information';
+  var errorElement = $('<footer>', { class: 'error generated', text: errorMessage });
+  if ( $('.error').length == 0 ) {
+    errorElement.hide().appendTo('body').fadeIn(fadeTime);
+  }
 }
 
-function reloadContent() {
+$(function() {
+  loadContent();
   setInterval(loadContent, refreshTime);
-}
-
-document.addEventListener("DOMContentLoaded", loadContent);
-document.addEventListener("DOMContentLoaded", reloadContent);
+});
